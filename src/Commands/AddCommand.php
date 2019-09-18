2bfaf4f9eadaf8fe9da02b295727ac3883b6a5e7
Ivan Kapshtyk <ivan.kapshtyk@herobrands.com.au>
2019-09-18 13:37:06 +0300

    Removed absolute path from LOCK_FILE in .gitignore

--- src/Commands/AddCommand.php
+++ src/Commands/AddCommand.php
@@ -2,6 +2,7 @@
 
 namespace BrainMaestro\GitHooks\Commands;
 
+use BrainMaestro\GitHooks\Hook;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 
@@ -132,7 +133,7 @@ class AddCommand extends Command
         $return = strpos($contents, $this->lockFile);
 
         if ($return === false) {
-            file_put_contents('.gitignore', $this->lockFile . PHP_EOL, FILE_APPEND);
+            file_put_contents('.gitignore', Hook::LOCK_FILE . PHP_EOL, FILE_APPEND);
             $this->debug("Added [{$this->lockFile}] to .gitignore");
         }
     }
