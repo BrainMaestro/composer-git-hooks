a2f5864c822cf6a27c8fe83a593c59e7469301f1
André Hoendgen <934583+ahoendgen@users.noreply.github.com>
2019-12-02 10:45:35 +0100

    Allow symfony 5 components (#102)

--- src/Commands/HookCommand.php
+++ src/Commands/HookCommand.php
@@ -13,7 +13,7 @@ class HookCommand extends SymfonyCommand
 
     public function __construct($hook, $contents)
     {
-        $this->hook = $hook;
+        $this->hook     = $hook;
         $this->contents = $contents;
         parent::__construct();
     }
@@ -24,12 +24,19 @@ class HookCommand extends SymfonyCommand
             ->setName($this->hook)
             ->setDescription("Test your {$this->hook} hook")
             ->setHelp("This command allows you to test your {$this->hook} hook")
-        ;
+            ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $contents = is_array($this->contents) ? implode(PHP_EOL, $this->contents) : $this->contents;
-        $output->writeln(shell_exec($contents));
+
+        $outputMessage = [];
+        $returnCode    = 0;
+        exec($contents, $outputMessage, $returnCode);
+
+        $output->writeln(implode(PHP_EOL, $outputMessage));
+
+        return $returnCode;
     }
 }
